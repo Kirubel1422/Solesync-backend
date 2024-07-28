e6b8cbc6bdd3175a8febf8022dbaa74a6deb8fa7
Kirubel <kirubel739@gmail.com>
2024-07-28 13:58:25 +0400

    feat(errorhandler.js): json format edit

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,11 @@
+const logger = require("../utils/logger")("error-handler");
 module.exports = (err, req, res, next) => {
+  logger.error(err);
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error";
-  res.status(statusCode).send(message);
+  const stack = err.stack || "Internal server error";
+  res.status(statusCode).send({
+    status: "error",
+    statusCode,
+    stack,
+  });
 };
