1d99919f4da0ba46164f579d0b9f7da420233ef3
Kirubel <kirubel739@gmail.com>
2024-07-28 16:42:59 +0400

    feat(auth.middleware): logging more descriptive

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const passport = require("passport");
+const logger = require("../utils/logger")("auth.middleware");
 
 exports.admin = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user, info) => {
@@ -6,10 +7,12 @@ exports.admin = (req, res, next) => {
       return next(err);
     }
     if (!user) {
+      logger.info("Unauthorized access request");
       return res.status(401).json({ message: "Unauthorized" });
     }
 
     if (user.role != "admin") {
+      logger.info("Forbidden access request by user");
       return res.status(403).json({ message: "Forbidden" });
     }
 
@@ -25,10 +28,12 @@ exports.user = (req, res, next) => {
     }
 
     if (!user) {
+      logger.info("Unauthorized access request");
       return res.status(401).json({ message: "Unauthorized" });
     }
 
     if (user.role != "user") {
+      logger.info("Forbidden access request by admin");
       return res.status(403).json({ message: "Forbidden" });
     }
 
