cafb59469ea9822ad0e3c4d3162eb4d1917e26bd
Kirubel <kirubel739@gmail.com>
2024-07-29 00:43:54 +0400

    feat(order.controller): search functionality

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -4,6 +4,7 @@ const logger = require("../utils/logger")("order.controller");
 exports.getAllOrders = (req, res, next) => {
   Order.find()
     .populate("products")
+    .populate("user")
     .then((orders) => res.json(orders))
     .catch((err) => {
       logger.error(err);
@@ -22,7 +23,7 @@ exports.getOrderById = (req, res, next) => {
 };
 
 exports.getSelfOrder = (req, res, next) => {
-  Order.find({ userId: req.body.userId })
+  Order.find({ user: req.body.user })
     .then((orders) => {
       res.json(orders);
     })
@@ -54,6 +55,56 @@ exports.updateOrder = (req, res, next) => {
 exports.deleteOrder = (req, res, next) => {
   Order.findByIdAndDelete(req.params.id)
     .then(() => res.status(204).end())
+    .catch((err) => {
+      logger.error(err);
+      next(err);
+    });
+};
+
+exports.searchByDate = (req, res, next) => {
+  const { date } = req.query;
+
+  const searchDate = new Date(date);
+  searchDate.setHours(0, 0, 0, 0);
+
+  const endDate = new Date(searchDate);
+  endDate.setHours(23, 59, 59, 99);
+
+  Order.find({ createdAt: { $gt: searchDate, $lt: endDate } })
+    .then((order) => {
+      res.json(order);
+    })
+    .catch((err) => {
+      logger.error(err);
+      next(err);
+    });
+};
+
+exports.search = (req, res, next) => {
+  const { findBy, value } = req.query;
+  Order.find()
+    .populate({
+      path: "user",
+      match: { [findBy]: { $regex: new RegExp(value, "i") } },
+    })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      logger.error(err);
+      next(err);
+    });
+};
+
+exports.filterByPage = (req, res, next) => {
+  const { page, limit } = req.query;
+
+  Order.find({})
+    .skip(limit * (page - 1))
+    .limit(limit)
+    .then((result) => {
+      res.json(result);
+    })
     .catch((err) => {
       logger.error(err);
       next(err);
