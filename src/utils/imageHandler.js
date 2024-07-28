1369846f1c1f251fa7bca158b3375990f5c2b761
Kirubel <kirubel739@gmail.com>
2024-07-28 16:44:29 +0400

    fix(imagehandler.js): bug fix

--- src/utils/imageHandler.js
+++ src/utils/imageHandler.js
@@ -1,21 +1,51 @@
 const crypto = require("crypto");
 const path = require("path");
+const logger = require("../utils/logger")("imageHandler");
+
 module.exports = (req, res, next) => {
-  const imgFiles = req.files;
+  const { imgFiles } = req.files;
   let images = [];
 
-  for (const img of imgFiles) {
-    if (!img || !path.extname(img.originalname).match(/\.(jpg|jpeg|png)$/i)) {
-      return res.status(400).json({ message: "Incorrect mime type." });
+  if (imgFiles.length === undefined) {
+    if (
+      !path.extname(imgFiles.name) ||
+      !path.extname(imgFiles.name).match(/\.(jpg|jpeg|png)$/i)
+    ) {
+      const err = new Error("Incorrect mime type.");
+      err.statusCode = 400;
+      next(err);
     }
-
-    const name = crypto.randomUUID() + path.extname(img.originalname);
-    img.mv(path.resolve(__dirname, `../public/images/${name}`), (err) => {
-      if (err) {
-        return res.status(500).json({ message: "Error uploading image" });
+    const name = crypto.randomUUID() + path.extname(imgFiles.name);
+    console.log(name);
+    imgFiles.mv(
+      path.resolve(__dirname, `../../public/images/${name}`),
+      (err) => {
+        if (err) {
+          const err = new Error("Error uploading image");
+          next(err);
+        }
       }
-    });
-    images.push(name);
+    );
+
+    images.push(`image/getImage/${name}`);
+  } else {
+    for (const img of imgFiles) {
+      if (!img || !path.extname(img.name).match(/\.(jpg|jpeg|png)$/i)) {
+        const err = new Error("Incorrect mime type.");
+        err.statusCode = 400;
+        next(err);
+      }
+
+      const name = crypto.randomUUID() + path.extname(img.name);
+      img.mv(path.resolve(__dirname, `../public/images/${name}`), (err) => {
+        if (err) {
+          const err = new Error("Error uploading image");
+          next(err);
+        }
+      });
+
+      images.push(`image/getImage/${name}`);
+    }
   }
 
   req.body.images = images;
