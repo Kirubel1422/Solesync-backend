e9226c78622f4b7793ba77929b8a2116c6c0f711
Kirubel <kirubel739@gmail.com>
2024-08-09 02:36:06 +0400

    feat(auth.router.js): added updateProfile router

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,8 +1,13 @@
-const { signup, signin } = require("../controllers/auth.controller");
+const {
+  signup,
+  signin,
+  updateProfile,
+} = require("../controllers/auth.controller");
+const { either } = require("../middlewares/auth.middleware");
 
 const router = require("express").Router();
 
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
-
+router.route("/updateProfile").put(either, updateProfile);
 module.exports = router;
