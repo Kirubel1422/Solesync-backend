54b626858c302bca56760033624929414218c8f1
Kirubel <kirubel739@gmail.com>
2024-08-13 02:55:21 +0400

    chore(errorhandler.js): updated error display on console

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const logger = require("../utils/logger")("error-handler");
 module.exports = (err, req, res, next) => {
   logger.error(err);
-  console.log(err);
+  console.log(err.stack);
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
   res.status(statusCode).send({
